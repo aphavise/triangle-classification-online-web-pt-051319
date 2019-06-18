200ef0af116a8205c0364eb8a46d2c82b40c1435
ANDREW PHAVISETH <andrewphaviseth@gmail.com>
2019-06-18 05:59:09 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -13,6 +13,22 @@ class Triangle
     
   end
   
+  def kind
+    validate_triangle
+    if x == y && y == z
+      :equilateral
+    elsif x == y || y == z || x == z
+      :isosceles
+    else
+      :scalene
+    end
+  end
+
+  # def validate_triangle
+  #   real_triangle = [(a + b > c), (a + c > b), (b + c > a)]
+  #   [a, b, c].each { |s| real_triangle << false if s <= 0 }
+  #   raise TriangleError if real_triangle.include?(false)
+  # end
   
   
   
